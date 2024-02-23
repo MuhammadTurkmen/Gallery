977acc0096e6cf11ac2037bfe4aadb6e74c61ab1
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-02-23 13:59:58 +0430

    init

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ function Gallery(element) {
 Gallery.prototype.openModal = function(selectiedImage, list) {
   this.setMainImage(selectiedImage)
   this.modalImages.innerHTML = list.map(function(image) {
-    return `<img src=${image.src}/>`
+    return `<img src="${image.src}" title="${image.title}"/>`
   }).join('')
   this.modal.classList.add('open')
 }
