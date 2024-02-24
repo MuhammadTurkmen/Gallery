ef3736199c78aa00ce42d4ed6bd7f6a9aa34d8fe
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-02-24 15:00:20 +0430

    init

--- app-proto.js
+++ app-proto.js
@@ -66,6 +66,15 @@ class Gallery {
         this.prevBtn.removeEventListener('click', this.prevImage)
         this.modalImages.removeEventListener('click', this.chooseImage)
     }
+
+    nexImage() {
+        const selected = this.modalImages.querySelector('.selected')
+        const next = selected.nextElementSibling || this.modalImages.firstElementChild
+        selected.classList.remove('selected')
+        next.classList.add('selected')
+        this.setMainImage(next)
+      }
+
 }
 
 
