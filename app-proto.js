3b58a79e53aec41cc35ccf2f94836e564f47e167
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-02-24 14:55:43 +0430

    init

--- app-proto.js
+++ app-proto.js
@@ -9,5 +9,28 @@ function getElement(selection) {
 }
 
 class Gallery {
-    
+    this.container = element
+  this.list = [...element.querySelectorAll('.img')]
+  // target
+  this.modal = getElement('.modal')
+  this.modalImg = getElement('.main-img')
+  this.imageName = getElement('.image-name')
+  this.modalImages = getElement('.modal-images')
+  this.closeBtn = getElement('.close-btn')
+  this.nextBtn = getElement('.next-btn')
+  this.prevBtn = getElement('.prev-btn')
+  // self refrence
+  // let self = this 
+  // bind functions 
+  // this.openModal = this.openModal.bind(this)
+  this.closeModal = this.closeModal.bind(this)
+  this.nexImage = this.nexImage.bind(this)
+  this.prevImage = this.prevImage.bind(this)
+  this.chooseImage = this.chooseImage.bind(this)
+  // container event
+  this.container.addEventListener('click', function(e) {
+    if(e.target.classList.contains('img')) {
+      this.openModal(e.target, this.list)
+    }
+  }.bind(this))
 }
